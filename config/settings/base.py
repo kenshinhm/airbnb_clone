950a97940639f801e5993b32851762a6291a4452
jaehyoungyoo <kenshinhm@naver.com>
2019-06-23 17:15:49 +0900

    add other static files

--- config/settings/base.py
+++ config/settings/base.py
@@ -158,7 +158,7 @@ STATICFILES_DIRS = [
     # os.path.join(ROOT_DIR, 'static'),
     #os.path.join(APPS_DIR, 'static'),
     str(ROOT_DIR.path("frontend", "build", "static")),
-    # str(ROOT_DIR.path("frontend", "build")),
+    str(ROOT_DIR.path("frontend", "build")),
 ]
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
 STATICFILES_FINDERS = [
