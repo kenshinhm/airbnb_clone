39aec1dc020f639c2aa0dc69a987e300515179f1
jaehyoungyoo <kenshinhm@naver.com>
2019-06-24 01:05:42 +0900

    deploy

--- load_database.py
+++ load_database.py
@@ -10,42 +10,48 @@ django.setup()
 from airbnb.rooms.models import Room, RoomPhoto
 from airbnb.users.models import User
 
-with open('rooms.pkl', 'rb') as f_rooms:
-
-    rooms_pickle = pickle.load(f_rooms)
-
-    # DB create
-    db_host = User.objects.get(username='admin')
-
-    for room in rooms_pickle:
-
-        Room.objects.create(name=room.name,
-                            city=room.city,
-                            location=room.location,
-                            type=room.type,
-                            capacity=room.capacity,
-                            bedroom=room.bedroom,
-                            bathroom=room.bathroom,
-                            bed=room.bed,
-                            summary=room.summary,
-                            room_info_0=room.room_info_0,
-                            room_info_1=room.room_info_1,
-                            room_info_2=room.room_info_2,
-                            room_info_3=room.room_info_3,
-                            price=room.price,
-                            host=db_host,
-                            lat=room.lat,
-                            lng=room.lng,
-                            )
-
-    print('Room DB Finish')
-
-# with open('room_photos.pkl', 'rb') as f_room_photos:
+# with open('rooms.pkl', 'rb') as f_rooms:
 #
-#     room_photos_pickle = pickle.load(f_room_photos)
-#     photos = room_photos_pickle.filter(room__id=room.id)
+#     rooms_pickle = pickle.load(f_rooms)
 #
-#     for photo in photos:
-#         RoomPhoto.objects.create(room=rm, photo=photo.photo)
+#     # DB create
+#     db_host = User.objects.get(username='admin')
+#
+#     for room in rooms_pickle:
+#
+#         Room.objects.create(name=room.name,
+#                             city=room.city,
+#                             location=room.location,
+#                             type=room.type,
+#                             capacity=room.capacity,
+#                             bedroom=room.bedroom,
+#                             bathroom=room.bathroom,
+#                             bed=room.bed,
+#                             summary=room.summary,
+#                             room_info_0=room.room_info_0,
+#                             room_info_1=room.room_info_1,
+#                             room_info_2=room.room_info_2,
+#                             room_info_3=room.room_info_3,
+#                             price=room.price,
+#                             host=db_host,
+#                             lat=room.lat,
+#                             lng=room.lng,
+#                             )
+#
+#     print('Room DB Finish')
+
+with open('room_photos.pkl', 'rb') as f_room_photos:
+
+    room_photos_pickle = pickle.load(f_room_photos)
+
+    photo = room_photos_pickle[2500]
+
+    room = Room.objects.get(name=photo.room.name)
+
+    RoomPhoto.objects.create(room=room, photo=photo.photo)
+
+    # for photo in room_photos_pickle:
+    #     print(photo)
+        # RoomPhoto.objects.create(room=rm, photo=photo.photo)
 
 
