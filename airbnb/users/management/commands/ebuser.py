13117e31ef47990f28b1ac8cf8f30c85fc174dcc
jaehyoungyoo <kenshinhm@naver.com>
2019-06-23 21:26:56 +0900

    remove 03_createsuperuser

--- airbnb/users/management/commands/ebuser.py
+++ airbnb/users/management/commands/ebuser.py
@@ -1,6 +1,9 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 
+# 03_createsuperuser:
+#   command: "source /opt/python/run/venv/bin/activate && python manage.py ebuser"
+
 
 class Command(BaseCommand):
 
