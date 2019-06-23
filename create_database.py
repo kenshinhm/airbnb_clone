9fae777fe1c4fa35b3ec76cbf435fbc7d2458275
jaehyoungyoo <kenshinhm@naver.com>
2019-06-23 23:56:45 +0900

    test roomphotos

--- create_database.py
+++ create_database.py
@@ -8,12 +8,16 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 django.setup()
 
-from airbnb.rooms.models import Room
+from airbnb.rooms.models import Room, RoomPhoto
 
 rooms = Room.objects.all()
+room_photos = RoomPhoto.objects.all()
 
 with open('rooms.pkl', 'wb') as f:
     pickle.dump(rooms, f)
+
+with open('room_photos.pkl', 'wb') as f:
+    pickle.dump(room_photos, f)
 
 print('Rooms dump 완료')
 
--- load_database.py
+++ load_database.py
@@ -7,34 +7,43 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 django.setup()
 
-from airbnb.rooms.models import Room
+from airbnb.rooms.models import Room, RoomPhoto
 from airbnb.users.models import User
 
-with open('rooms.pkl', 'rb') as f:
-    rooms = pickle.load(f)
+with open('rooms.pkl', 'rb') as f_rooms:
 
-    # DB create
-    db_host = User.objects.get(username='admin')
+    with open('room_photos.pkl', 'rb') as f_room_photos:
 
-    room = rooms[0]
-    Room.objects.create(name=room.name,
-                        city=room.city,
-                        location=room.location,
-                        type=room.type,
-                        capacity=room.capacity,
-                        bedroom=room.bedroom,
-                        bathroom=room.bathroom,
-                        bed=room.bed,
-                        summary=room.summary,
-                        room_info_0=room.room_info_0,
-                        room_info_1=room.room_info_1,
-                        room_info_2=room.room_info_2,
-                        room_info_3=room.room_info_3,
-                        price=room.price,
-                        host=db_host,
-                        lat=room.lat,
-                        lng=room.lng,
-                        )
+        rooms_pickle = pickle.load(f_rooms)
+        room_photos_pickle = pickle.load(f_room_photos)
 
-    print('Room DB Finish')
+        # DB create
+        db_host = User.objects.get(username='admin')
+
+        room = rooms_pickle.filter(city='서울특별시')[0]
+        photos = room_photos_pickle.filter(room__id=room.id)
+
+        rm = Room.objects.create(name=room.name,
+                                 city=room.city,
+                                 location=room.location,
+                                 type=room.type,
+                                 capacity=room.capacity,
+                                 bedroom=room.bedroom,
+                                 bathroom=room.bathroom,
+                                 bed=room.bed,
+                                 summary=room.summary,
+                                 room_info_0=room.room_info_0,
+                                 room_info_1=room.room_info_1,
+                                 room_info_2=room.room_info_2,
+                                 room_info_3=room.room_info_3,
+                                 price=room.price,
+                                 host=db_host,
+                                 lat=room.lat,
+                                 lng=room.lng,
+                                 )
+
+        for photo in photos:
+            RoomPhoto.objects.create(room=rm, photo=photo)
+
+        print('Room DB Finish')
 
