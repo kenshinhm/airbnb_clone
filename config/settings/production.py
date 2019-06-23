704a0c4177a7e2c026118e0e0ea062ec8827f861
jaehyoungyoo <kenshinhm@naver.com>
2019-06-23 12:39:54 +0900

    setting on production

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,10 +3,7 @@ from .base import env
 import django_heroku
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
-SECRET_KEY = env(
-    "DJANGO_SECRET_KEY",
-    default="hLKwXQmw9JsheNR3lTrsNjoWPKNuVKmXfTw8iZM40jZ7UrFcZLsx3gtwBwj5Romc",
-)
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["localhost", "airbnb-clone-v1.herokuapp.com", "127.0.0.1"]
 
@@ -22,7 +19,7 @@ ALLOWED_HOSTS = ["localhost", "airbnb-clone-v1.herokuapp.com", "127.0.0.1"]
 #
 # # DATABASES
 # # ------------------------------------------------------------------------------
-# DATABASES["default"] = env.db("DATABASE_URL")  # noqa F405
+DATABASES["default"] = env.db("DATABASE_URL")  # noqa F405
 # DATABASES["default"]["ATOMIC_REQUESTS"] = True  # noqa F405
 # DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)  # noqa F405
 #
